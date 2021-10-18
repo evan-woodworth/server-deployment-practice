f0e541c2113eaa66d112438148b3fb62fca0f466
Evan Woodworth <evan.woodworth@gmail.com>
2021-10-18 16:17:36 -0700

    bug fix: port resolution

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 const app = require('./app.js');
 
-app.listen(3000, () => {
-    console.log('App is running on port 3000');
-})
+// for deployment, we need to let heoku set the port
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log('App is running on port ', port);
+});
